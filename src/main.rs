a0114feeb6791177b2d3e1935655eb6cce865934
ygnid <ygnid@icloud.com>
2023-11-14 10:18:01 +0800

    add floor and camera

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use std::arch::x86_64::_SIDD_LEAST_SIGNIFICANT;
+
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {}, {}", position.x, position.y);
+    }
+}
+
+struct Entity(u64);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn hello_world() {
+    println!("Hello, world!");
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+fn main() {
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, (spawn_floor, spawn_camera, add_people))
+            .add_systems(Update, greet_people);
+    }
+}
+
+fn spawn_floor(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane::from_size(15.0))),
+        material: materials.add(Color::DARK_GREEN.into()),
+        ..default()
+    };
+    commands.spawn(floor);
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let camera = Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    };
+
+    commands.spawn(camera);
+}
