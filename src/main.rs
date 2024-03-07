d2486ae28cace06a33d52fdabf8db4c877e47db8
ygnid <ygnid@icloud.com>
2024-03-07 22:14:43 +0800

    add player_movement

--- src/main.rs
+++ src/main.rs
@@ -8,55 +8,8 @@ use player::PlayerPlugin;
 use camera::CameraPlugin;
 use world::WorldPlugin;
 
-#[derive(Component)]
-struct Position {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn print_position_system(query: Query<&Position>) {
-    for position in &query {
-        println!("position: {}, {}", position.x, position.y);
-    }
-}
-
-struct Entity(u64);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin, WorldPlugin, CameraPlugin, PlayerPlugin))
+        .add_plugins((DefaultPlugins, WorldPlugin, CameraPlugin, PlayerPlugin))
         .run();
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
-            .add_systems(Update, greet_people);
-    }
 }
--- src/player.rs
+++ src/player.rs
@@ -4,19 +4,68 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player);
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, player_movement);
     }
 }
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Speed(f32);
+
+fn player_movement(
+    keys: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut player_q: Query<(&mut Transform, &Speed), With<Player>>,
+    cam_q: Query<&Transform, (With<Camera3d>, Without<Player>)>,
+) {
+    for (mut player_transform , player_speed) in player_q.iter_mut() {
+        let cam = match cam_q.get_single() {
+            Ok(c) => c,
+            Err(e) => Err(format!("Error retrieving camera: {}", e)).unwrap(),
+        };
+        let mut direction = Vec3::ZERO;
+
+        //forward
+        if keys.pressed(KeyCode::W) {
+            direction += cam.forward();
+        }
+        //back
+        if keys.pressed(KeyCode::S) {
+            direction += cam.back();
+        }
+        //left
+        if keys.pressed(KeyCode::A) {
+            direction += cam.left();
+        }
+        //back
+        if keys.pressed(KeyCode::D) {
+            direction += cam.right();
+        }
+        // println!("direction: {}", direction);
+
+        let movement = direction.normalize_or_zero() * player_speed.0 * time.delta_seconds();
+        // println!("delta_seconds: {}", time.delta_seconds());
+        player_transform.translation += movement;
+    }
+}
+
 fn spawn_player(
     mut commands: Commands,
     mut meshs: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let player = PbrBundle {
-        mesh: meshs.add(Mesh::from(shape::Cube::new(1.0))),
-        material: materials.add(Color::BLUE.into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..Default::default()
-    };
+    let player = (
+        PbrBundle {
+            mesh: meshs.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(Color::BLUE.into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..Default::default()
+        },
+        Speed(5.0),
+        Player,
+    );
     commands.spawn(player);
 }
